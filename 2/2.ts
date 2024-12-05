548881067c068114d741178a5231e074fe553958
nftbiy <nftbiy@gmail.com>
2024-12-05 09:20:25 -0800

    First commit

--- 2/2.ts
+++ 2/2.ts
@@ -1,27 +1,31 @@
 /* --- Day 2: Red-Nosed Reports --- */
+/* https://adventofcode.com/2024/day/2*/
+
 const fs = require('fs')
 
-const reports = fs
+type Report = number[]
+
+const reports: Report[] = fs
 	.readFileSync('input.txt', 'utf-8')
 	.split('\n')
 	.map((row) => row.split(' ').map((v) => +v))
 
-const isInRange = (arr, i) => {
+const isInRange = (arr: number[], i: number) => {
 	return (
 		Math.abs(arr[i] - arr[i + 1]) > 0 && Math.abs(arr[i] - arr[i + 1]) <= 3
 	)
 }
 
-const isContinuousDirection = (arr, i) => {
+const isContinuousDirection = (arr: number[], i: number) => {
 	if (i === 0) i++
 	if (arr[i - 1] > arr[i] && arr[i] > arr[i + 1]) return true
 	if (arr[i - 1] < arr[i] && arr[i] < arr[i + 1]) return true
 	return false
 }
 
-const filterReportStatus = (reports) => {
-	const unsafeReports = []
-	const safeReports = reports.reduce((acc, report) => {
+const filterReportStatus = (reports: Report[]) => {
+	const unsafeReports: Report[] = []
+	const safeReports: Report[] = reports.reduce((acc, report) => {
 		for (let i = 0; i < report.length - 1; i++) {
 			if (!isInRange(report, i) || !isContinuousDirection(report, i)) {
 				unsafeReports.push(report)
@@ -29,7 +33,7 @@ const filterReportStatus = (reports) => {
 			}
 		}
 		return [...acc, report]
-	}, [])
+	}, [] as Report[])
 	return { unsafeReports, safeReports }
 }
 
@@ -37,7 +41,7 @@ const { safeReports, unsafeReports } = filterReportStatus(reports)
 let safeCount: number = safeReports.length
 
 unsafeReports.forEach((report) => {
-	const newReports = []
+	const newReports: Report[] = []
 	for (let i = 0; i < report.length; i++) {
 		const newReport = report.filter((_, index) => i !== index)
 		newReports.push(newReport)
